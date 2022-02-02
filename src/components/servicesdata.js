25ee6d4dd7b1fcace253c04f18851852a51bca0f
ejoor emmanuel <ejooremmanuel@gmail.com>
2022-02-02 19:31:52 +0100

    new

--- src/components/servicesdata.js
+++ src/components/servicesdata.js
@@ -3,37 +3,37 @@ const sevicesData = [
     id: "1",
     heading: "Business Process Re-Engineering",
     icon: "engineering",
-    desc: "  Nobis nam facilis aperiam. Amet voluptatum quia nesciunt nemo id enim ipsam doloribus debitis expedita quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error.  Nobis nam facilis aperiam. Amet voluptatum quia nesciunt nemo id enim ipsam doloribus debitis expedita quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error.",
+    desc: " quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error. quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error.",
   },
   {
     id: "2",
     heading: "Cost Management for Operations and Projects",
     icon: "calculate",
-    desc: "  Nobis nam facilis aperiam. Amet voluptatum quia nesciunt nemo id enim ipsam doloribus debitis expedita quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error.  Nobis nam facilis aperiam. Amet voluptatum quia nesciunt nemo id enim ipsam doloribus debitis expedita quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error.",
+    desc: " quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error. quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error.",
   },
   {
     id: "3",
     heading: "Project and Business Funding",
     icon: "euro",
-    desc: "  Nobis nam facilis aperiam. Amet voluptatum quia nesciunt nemo id enim ipsam doloribus debitis expedita quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error.  Nobis nam facilis aperiam. Amet voluptatum quia nesciunt nemo id enim ipsam doloribus debitis expedita quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error.",
+    desc: " quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error. quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error.",
   },
   {
     id: "4",
     heading: "Contract and Contracts Agreements",
     icon: "article",
-    desc: "  Nobis nam facilis aperiam. Amet voluptatum quia nesciunt nemo id enim ipsam doloribus debitis expedita quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error.  Nobis nam facilis aperiam. Amet voluptatum quia nesciunt nemo id enim ipsam doloribus debitis expedita quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error.",
+    desc: " quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error. quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error.",
   },
   {
     id: "5",
     heading: "Financial Sytems and Controls",
     icon: "calculate",
-    desc: "  Nobis nam facilis aperiam. Amet voluptatum quia nesciunt nemo id enim ipsam doloribus debitis expedita quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error.  Nobis nam facilis aperiam. Amet voluptatum quia nesciunt nemo id enim ipsam doloribus debitis expedita quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error.",
+    desc: " quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error. quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error.",
   },
   {
     id: "6",
     icon: "auto_fix_high",
     heading: "Tax Management",
-    desc: "  Nobis nam facilis aperiam. Amet voluptatum quia nesciunt nemo id enim ipsam doloribus debitis expedita quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error.  Nobis nam facilis aperiam. Amet voluptatum quia nesciunt nemo id enim ipsam doloribus debitis expedita quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error.",
+    desc: " quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error. quaerat earum, quidem molestias, quod quasi hic eius? Modi voluptates placeat laborum impedit possimus consectetur dolorem error.",
   },
 ];
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -21,10 +21,7 @@ const About = () => {
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt in
             beatae debitis quibusdam non quod quia eius, dignissimos rem magnam
             quis fugit libero id, illo inventore ipsam ab accusantium! Facilis
-            maiores illum molestias iusto corrupti laboriosam! In natus suscipit
-            dolorum, repudiandae quisquam dignissimos perspiciatis beatae
-            ratione debitis illum quas necessitatibus. Lorem, ipsum dolor sit
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt in
+            maiores illum molestias iusto corrupti laboriosam
           </p>
           <Link className="about__link" to="/about">
             Learn More
