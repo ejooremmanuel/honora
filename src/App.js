b1c695039e1e72fcf0efcd495730b33f373e622c
ejoor emmanuel <ejooremmanuel@gmail.com>
2022-02-02 22:01:45 +0100

    new change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Service from "./components/Service";
-import Footer from "./components/Footer";
 import AboutComponent from "./components/AboutComponent";
 import Contact from "./pages/contact";
 import { Toaster } from "react-hot-toast";
